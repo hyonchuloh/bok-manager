8fe9ba43998b9522ba1a38282e2063293ef71c19
2310449 <hyonchul.oh@bok.or.kr>
2024-06-23 04:00:08 +0900

    init project

---
+++ src/main/java/com/bok/iso/bok_manager/BokManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bok.iso.bok_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bok/iso/bok_manager/BokManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bok.iso.bok_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
