2f79120ec13ea1e5df84401ba5f35344c6a3817d
hc5642 <hc5642@gmail.com>
2024-07-26 09:30:51 +0900

    pid 생성하도록 변경

--- src/main/java/com/bok/iso/mngr/BokManagerApplication.java
+++ src/main/java/com/bok/iso/mngr/BokManagerApplication.java
@@ -5,6 +5,7 @@ import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,9 @@ import org.springframework.context.annotation.Bean;
 public class BokManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BokManagerApplication.class, args);
+        SpringApplication application = new SpringApplication(BokManagerApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
 	}
 
 	@Bean
